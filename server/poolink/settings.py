008770629aa9664e75facf88ff32806201ed6a25
tigermeal <tigermeal0125@gmail.com>
2019-10-26 21:56:58 +0900

    :wrench: Encoding issue

--- server/poolink/settings.py
+++ server/poolink/settings.py
@@ -136,9 +136,9 @@ AUTENTICATION_BACKENDS = (
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
