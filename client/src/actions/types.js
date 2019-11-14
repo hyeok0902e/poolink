98f2dbdfdf085e98a9e25c3488da8564c3bc2caa
tigermeal <tigermeal0125@gmail.com>
2019-11-14 17:59:12 +0900

    :wrench: Types naming change

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,4 @@
-export const AUTH_START = 'AUTH_START';
-export const AUTH_SUCCESS = 'AUTH_SUCCESS';
-export const AUTH_FAIL = 'AUTH_FAIL';
-export const AUTH_LOGOUT = 'AUTH_LOGOUT';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
