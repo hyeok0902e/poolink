8275ed602de3c4f520923de6a2bf5d011d93821c
tigermeal <tigermeal0125@gmail.com>
2019-11-20 21:09:55 +0900

    :sparkles: Post List pagination

--- client/src/components/postlist/index.js
+++ client/src/components/postlist/index.js
@@ -2,10 +2,22 @@ import React, {Component} from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import { Container, Button, Typography } from '@material-ui/core';
+import { Container, Button, Typography, CircularProgress, Paper, Table, TableHead, TableRow, TableCell, TableBody, Hidden } from '@material-ui/core';
+import './styles.css';
 
 
 class PostList extends Component {
+  state = {
+    currentPage: 1,
+    perPage: 15
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      currentPage: Number(e.target.id)
+    });
+  } 
+
   btnHandle = () => {
     this.props.history.push('/create');
   }
@@ -13,8 +25,26 @@ class PostList extends Component {
   render() {
     const {
       posts,
-      isAuthenticated
+      isAuthenticated,
     } = this.props;
+    
+    const currentPage = this.state.currentPage;
+    const perPage = this.state.perPage;
+
+    const indexLastPost = currentPage * perPage;
+    const indexFirstPost = indexLastPost - perPage;
+    const currentPosts = posts.slice(indexFirstPost, indexLastPost);
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(posts.length / perPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <button key={number} id={number} onClick={this.handleClick}>{number}</button>
+      );
+    });
 
     const currentUser = localStorage.getItem('username')
     
@@ -32,27 +62,44 @@ class PostList extends Component {
         align='center'
         component='p'
         >
-          회원만 글을 쓸 수 있습니다.
+        회원만 글을 쓸 수 있습니다.
       </Typography>
     )
 
-
     const postList = posts.length ? (
-      posts.map(post => {
-        return (
-          <Container maxWidth='lg' className="post" key={post.id}>
-            <div className="post-content">
-              <Link to={'/' + post.id}>
-              <h1 className="post-title">{post.title}</h1>
-              </Link>
-              <p>{post.content}</p>
-              <p>{moment(post.created_at).fromNow()}</p>
-            </div>
-          </Container>
-        )
-      })
+        <Paper className='postWrapper'>
+          <Table className='postTable'>
+            <TableHead>
+              <TableRow>
+                <TableCell>제목</TableCell>
+                <Hidden xsDown>
+                  <TableCell align="center">에디터</TableCell>
+                </Hidden>
+                <TableCell align="center">날짜</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {currentPosts.map(post => {
+                return(
+                  <TableRow>
+                  <TableCell>
+                    <Link to={'/' + post.id} className="Link">{post.title}</Link>
+                  </TableCell>
+                  <Hidden xsDown>
+                    <TableCell width='10%' align="center">{post.user.username}</TableCell>
+                  </Hidden>
+                  
+                  <TableCell width='10%' align="center">{moment(post.created_at).format('MM.DD')}</TableCell>
+                </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
     ) : (
-      <div className="center">No posts yet</div>
+      <Container maxWidth='sm' align='center'>
+        <CircularProgress className='progress'/>
+      </Container>
     )
     
     return (
@@ -62,8 +109,10 @@ class PostList extends Component {
           maxWidth='lg'>
           {postCreateBtn}
         </Container>
-        
         {postList}
+        <ul id="page-numbers" align='center'>
+          {renderPageNumbers}
+        </ul>
       </Container>
     )
   }
